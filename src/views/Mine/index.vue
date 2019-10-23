eeb6291c51af5eba7abffa652ffd575a965bb97b
unknown <670661447@qq.com>
2019-10-23 14:58:52 +0800

    style

--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,19 +1,24 @@
 <template>
-    <div>
-        <Header title="我的喵喵"></Header>
-        <TabBar></TabBar>
+  <div>
+    <Header title="我的喵喵"></Header>
+    <div id="content">
+      <Login/>
     </div>
+    <TabBar></TabBar>
+  </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
 import TabBar from "@/components/TabBar";
+import Login from "@/components/Login";
 
 export default {
   name: "Mine",
   components: {
     Header,
-    TabBar
+    TabBar,
+    Login
   }
 };
 </script>
